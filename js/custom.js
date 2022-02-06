60c5779d59a89553f7908afa0c5c9d8a35084d5c
Neshanth <m.neshanth13@gmail.com>
2022-02-06 13:23:40 +0530

    added mobile nav and categories

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+let hamburgerIcon = document.querySelector(".hamburger-icon");
+let mobileNav = document.querySelector(".mobile-navigation-container");
+hamburgerIcon.addEventListener("click", () => {
+  if (mobileNav.dataset.toggle === "hide") {
+    mobileNav.style.minHeight = "7rem";
+    mobileNav.dataset.toggle = "show";
+  } else if (mobileNav.dataset.toggle === "show") {
+    mobileNav.style.minHeight = "0px";
+    mobileNav.dataset.toggle = "hide";
+  }
+});
+
+//Show Search Form
+let searchIcon = document.querySelector(".search-icon");
+searchIcon.addEventListener("click", () => {
+  document.querySelector(".search-modal").style.height = "100vh";
+});
+let closeIcon = document.querySelector(".close-icon");
+closeIcon.addEventListener("click", () => {
+  document.querySelector(".search-modal").style.height = "0vh";
+});
